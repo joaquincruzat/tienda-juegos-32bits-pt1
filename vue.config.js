627024659f11e1eb65456b6f0e63c78a940f40bb
Joaquín Cruzat <94878772+joaquincruzat@users.noreply.github.com>
2022-05-25 15:55:47 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/tienda-jeugos-32bits-pt1/" :'',
+  publicPath: process.env.NODE_ENV === "production" ? "/tienda-juegos-32bits-pt1/" :'',
 })
