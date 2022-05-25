75ddc78eacb10f96438cc03c62f2703a811690ac
Joaquín Cruzat <94878772+joaquincruzat@users.noreply.github.com>
2022-05-25 15:54:03 -0400

    Delete aaa.js

--- aaa.js
+++
@@ -1,8 +0,0 @@
-// async getAllGames(context) {
-//     try {
-//       const games = await Axios.get('/juegos.json');
-//       context.commit('SET_LIST', games);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   },
